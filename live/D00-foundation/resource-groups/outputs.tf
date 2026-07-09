dfffad0cf8764b757bb883fc61ceedfb905583c4
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-09 19:59:20 +0900

    Add D00 foundation outputs

---
+++ live/D00-foundation/resource-groups/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_group_names" {
+  value = module.dr_resource_groups.resource_group_names
+}
+
+output "resource_group_ids" {
+  value = module.dr_resource_groups.resource_group_ids
+}
