c568d1c98ac83c890c08df1a912f4dc9fc59d02a
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-09 21:19:08 +0900

    Add D10 DR hub outputs

---
+++ live/D10-platform/dr-hub-network-jpe/outputs.tf
@@ -0,0 +1,11 @@
+output "dr_hub_vnet_id" {
+  value = module.dr_hub.vnet_id
+}
+
+output "dr_hub_vnet_name" {
+  value = module.dr_hub.vnet_name
+}
+
+output "dr_hub_subnet_ids" {
+  value = module.dr_hub.subnet_ids
+}
