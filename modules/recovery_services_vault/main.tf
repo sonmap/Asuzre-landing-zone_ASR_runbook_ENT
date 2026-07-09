b3a6ed2b4a2d8bce6e4d80fede8c996532f3a763
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-09 19:43:26 +0900

    Add recovery services vault module

---
+++ modules/recovery_services_vault/main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_recovery_services_vault" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  soft_delete_enabled = var.soft_delete_enabled
+  tags                = var.tags
+}
