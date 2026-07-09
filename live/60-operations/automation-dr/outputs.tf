1bcaa9073d6ff7273ef271e80907fba33ab51256
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-09 19:47:16 +0900

    Add automation root outputs

---
+++ live/60-operations/automation-dr/outputs.tf
@@ -0,0 +1,11 @@
+output "automation_account_id" {
+  value = module.automation_dr.automation_account_id
+}
+
+output "automation_account_principal_id" {
+  value = module.automation_dr.automation_account_principal_id
+}
+
+output "runbook_names" {
+  value = module.automation_dr.runbook_names
+}
